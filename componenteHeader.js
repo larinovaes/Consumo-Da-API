3560ff292b7d337fa1287329f1d47ec2d8e1e1f5
Larissa Novais <larinovais54@gmail.com>
2022-01-11 14:23:47 -0300

    Fazer Header

---
+++ componenteHeader.js
@@ -0,0 +1,11 @@
+/*
+Criar um componente Header que recebe uma props e me retorna um header com a props sendo impressa no navegador
+*/
+
+import react from "react";
+
+function ComponenteHeader(props) {
+    return (
+        <header>Hello </header>
+    )
+}
